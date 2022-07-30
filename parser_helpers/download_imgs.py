d0c74d458aeb9def2f3dcc4fb3ac7220b2051bd0
Oliver K. Ernst <oernst@ucsd.edu>
2022-07-29 19:04:32 -0700

    Download image progress

--- parser_helpers/download_imgs.py
+++ parser_helpers/download_imgs.py
@@ -2,15 +2,27 @@ from .data import PostEntry
 import requests
 import os
 from typing import List, Union
+from dataclasses import dataclass
 
-def download_img(url: str, fname_dest: str):
-    print("Downloading entry: %s from %s" % (fname_dest, url))
+@dataclass
+class Context:
+    noentries: int
+    ientry: int = 0
+    iimage: int = 0
+    noimages: int = 0
+
+    @property
+    def progress(self) -> str:
+        return "(entry %04d/%04d) (img %02d/%02d)" % (self.ientry, self.noentries, self.iimage, self.noimages)
+
+def download_img(url: str, fname_dest: str, c: Context):
+    print("%s - downloading entry: %s from %s" % (c.progress, fname_dest, url))
 
     img_data = requests.get(url).content
     with open(fname_dest, 'wb') as handler:
         handler.write(img_data)
 
-def download_imgs_for_entry(entry: PostEntry, img_dirname: str):
+def download_imgs_for_entry(entry: PostEntry, img_dirname: str, c: Context):
     # Make directory
     if not os.path.isdir(img_dirname):
         os.makedirs(img_dirname)
@@ -20,16 +32,17 @@ def download_imgs_for_entry(entry: PostEntry, img_dirname: str):
     if not os.path.isdir(img_dirname_entry):
         os.makedirs(img_dirname_entry)
     
-    # Store
-    entry.img_dirname = img_dirname
-
     # Download
-    for img in entry.imgs:
+    c.noimages = len(entry.imgs)
+    for c.iimage, img in enumerate(entry.imgs):
         fname = os.path.join(img_dirname_entry, img.fname_wo_path)
-        download_img(img.url, fname)
+        download_img(img.url, fname, c)
 
 def download_imgs_from_md(entries: List[PostEntry], img_dirname: str, limit: Union[int,None] = None):
     if limit == None:
         limit = len(entries)
-    for entry in entries[:limit]:
-        download_imgs_for_entry(entry, img_dirname)
+
+    c = Context(noentries=limit)
+
+    for c.ientry, entry in enumerate(entries[:c.noentries]):
+        download_imgs_for_entry(entry, img_dirname, c)
